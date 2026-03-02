26fb303fedc71dfd3330d579c1f373c7948d3c00
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-02 11:28:08 +0100

    New rectangle component on ngx-svg-graphics

---
+++ projects/svg-graphics/src/lib/components/rectangle/rectangle.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+import {BoundingBox} from "../../models/bounding-box";
+
+@Component({
+  selector: '[rectangleG]',
+  imports: [],
+  templateUrl: './rectangle.component.svg',
+  styleUrl: './rectangle.component.css'
+})
+export class RectangleComponent {
+  @Input() position!: BoundingBox
+  @Input() color: string = '#ccffff';
+
+}
