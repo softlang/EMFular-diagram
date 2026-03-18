add15f089f7055d80f9e6b673556494170202c31
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-18 19:46:52 +0100

    Revert "Make dragger reset the Bounding bx so that updates see changes"
    This reverts commit e2d307d1d29970f8ca90558bcc0851759a5f6ac2.

--- projects/svg-graphics/src/lib/models/dragger.ts
+++ projects/svg-graphics/src/lib/models/dragger.ts
@@ -23,23 +23,12 @@ export class Dragger<T extends Positionable> {
     if (this.dragActive) {
       this.wasReallyDragged = true;
       event.preventDefault();
-
       const dragX = event.clientX;
-      const dragY = event.clientY;
-
-      const deltaX = dragX - this.dragStartX;
-      const deltaY = dragY - this.dragStartY;
-
-      // IMMUTABLE UPDATE
-      this.elem.position = {
-        ...this.elem.position,
-        x: this.elem.position.x + deltaX,
-        y: this.elem.position.y + deltaY
-      };
-
+      this.elem.position.x+= (dragX - this.dragStartX);
       this.dragStartX = dragX;
+      const dragY = event.clientY;
+      this.elem.position.y+= (dragY - this.dragStartY);
       this.dragStartY = dragY;
-
       return true;
     }
     return false;
