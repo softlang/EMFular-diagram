5f678512196bc3856e3753465b4c1c02fb5e258a
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-18 19:57:10 +0100

    Make arrow between boxes see changes on x and y and react

--- projects/svg-graphics/src/lib/components/arrows/arrow-between-boxes/arrow-between-boxes.component.ts
+++ projects/svg-graphics/src/lib/components/arrows/arrow-between-boxes/arrow-between-boxes.component.ts
@@ -1,6 +1,5 @@
 import { AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges,} from '@angular/core';
 import { Point } from "@angular/cdk/drag-drop";
-import {v4 as uuidv4} from "uuid";
 import {BoundingBox} from "../../../models/bounding-box";
 import {PathLayouter} from "../../../utils/path-layouter";
 import {ArrowBetweenPointsComponent} from "../arrow-between-points/arrow-between-points.component";
@@ -11,43 +10,42 @@ import {ArrowBetweenPointsComponent} from "../arrow-between-points/arrow-between
     styleUrl: './arrow-between-boxes.component.css',
     imports: [ArrowBetweenPointsComponent]
 })
-export class ArrowBetweenBoxesComponent implements OnChanges, AfterViewInit {
-  @Input() start!: BoundingBox;
-  @Input() end!: BoundingBox;
+export class ArrowBetweenBoxesComponent implements AfterViewInit {
+
+  private _start!: BoundingBox;
+  @Input() set start(bb: BoundingBox) {
+    this._start = bb;
+    if (this.positioned) this.computePositions();
+  }
+
+  private _end!: BoundingBox;
+  @Input() set end(bb: BoundingBox) {
+    this._end = bb;
+    if (this.positioned) this.computePositions();
+  }
+
   @Input() arrowType?: string;
   @Input() text?: string;
   @Input() style?: string;
 
+  x1 = 0;
+  y1 = 0;
+  x2 = 5;
+  y2 = 5;
 
-  x1: number = 0;
-  y1: number = 0;
-  x2: number = 5;
-  y2: number = 5;
+  positioned = false;
 
-  id = uuidv4();
-
-  positioned= false;
-
-  constructor(
-    private cdr: ChangeDetectorRef,
-    ) {}
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngAfterViewInit() {
-    this.computePositions()
+    this.computePositions();
     this.positioned = true;
     this.cdr.detectChanges();
   }
 
-  ngOnChanges() {
-    if(this.positioned) {
-      this.computePositions()
-    }
-  }
-
-
   private computePositions() {
-    let res = PathLayouter.bestPoints(this.start, this.end);
-    this.applyBestPoints(res)
+    const res = PathLayouter.bestPoints(this._start, this._end);
+    this.applyBestPoints(res);
   }
 
   private applyBestPoints(res: Point[]) {
@@ -56,5 +54,4 @@ export class ArrowBetweenBoxesComponent implements OnChanges, AfterViewInit {
     this.x2 = res[1].x;
     this.y2 = res[1].y;
   }
-
 }
