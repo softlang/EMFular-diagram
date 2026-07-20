4872ebca311c89a862c2b3569ef6f8e05b1b439c
Susanne Göbel <goebel@uni-koblenz.de>
2026-07-20 12:30:59 +0200

    Remove every spec that just tests creation

--- src/lib/models/dragger.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Dragger } from './dragger';
-
-describe('Dragger', () => {
-  it('should create an instance', () => {
-    let example = {gId: "id", position: { x: 1, y: 2, w: 2, h: 1 }};
-
-    expect(new Dragger(example)).toBeTruthy();
-  });
-});
--- src/lib/utils/path-layouter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PathLayouter } from './path-layouter';
-
-describe('PathLayouter', () => {
-  it('should create an instance', () => {
-    expect(new PathLayouter()).toBeTruthy();
-  });
-});
