5fd2db3cfcdf32060dd94713127c1b294a252222
Susanne Göbel <goebel@uni-koblenz.de>
2026-07-21 14:21:15 +0200

    Removing extra test files to verify setup and isolate mistakes, they do not belong into the lib

--- vitest-example/test-comp/test-comp.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { PlainComponent } from './test-comp.component';
-
-describe('plain', () => {
-    it('imports', () => {
-        expect(PlainComponent).toBeDefined();
-    });
-});
--- vitest-example/test-comp/test-comp.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'plain-test',
-    standalone: true,
-    template: 'hello'
-})
-export class PlainComponent {}
