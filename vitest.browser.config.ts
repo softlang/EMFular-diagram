2128f33f1db26ed906c3b425a7f48b67c70d9534
Susanne Göbel <goebel@uni-koblenz.de>
2026-07-21 13:22:08 +0200

    Angular19 compatible bridge plus adapted config - currently all tests fail

--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import { playwright } from '@vitest/browser-playwright'
+import angular from "@analogjs/vite-plugin-angular";
 
 export default defineConfig({
+  plugins: [
+    angular()
+  ],
   test: {
     setupFiles: ['./vitest.setup.ts'],
     browser: {
