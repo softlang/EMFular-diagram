6fce466bd6d6207a96cf9bb6eacc77e18cb78fc2
Susanne Göbel <goebel@uni-koblenz.de>
2026-07-22 12:19:18 +0200

    Remove extra browser config since this project will only use browser tests, hence copy browser config as standard config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
+import angular from "@analogjs/vite-plugin-angular";
+
+//we use only browser config since the lib is mostly for graphical components and hence heavily depends on the DOM
 
 export default defineConfig({
+    plugins: [
+        angular()
+    ],
     test: {
-        globals: true,
-        environment: 'jsdom',
-        coverage: {
-            reporter: ['text', 'html'],
-            reportsDirectory: './coverage'
-        }
-    }
-});
+        setupFiles: ['./vitest.setup.ts'],
+        browser: {
+            enabled: true,
+            provider: playwright(),
+            // https://vitest.dev/config/browser/playwright
+            instances: [
+                {browser: 'chromium'}
+            ],
+        },
+    },
+})
