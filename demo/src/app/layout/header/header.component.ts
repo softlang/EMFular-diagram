d67751ebf0fac5574f3242c6c2ba053c08679b8f
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-04 08:07:52 +0200

    Save vertical space by making nav sit next to header

--- demo/src/app/layout/header/header.component.ts
+++ demo/src/app/layout/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'demo-header',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgOptimizedImage
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
