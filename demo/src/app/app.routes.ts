675f51ee75af4062cd4a9130ed8cd53b439db4b9
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-04 11:29:07 +0200

    Routing on dummy components

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ArrowsComponent} from "./content/arrows/arrows.component";
+import {DragDropComponent} from "./content/drag-drop/drag-drop.component";
+import {HomeComponent} from "./content/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'arrows', component: ArrowsComponent },
+    { path: 'drag-drop', component: DragDropComponent }
+];
---
+++ demo/src/app/content/arrows/arrows.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-arrows',
+  imports: [],
+  templateUrl: './arrows.component.html',
+  styleUrl: './arrows.component.css'
+})
+export class ArrowsComponent {
+
+}
