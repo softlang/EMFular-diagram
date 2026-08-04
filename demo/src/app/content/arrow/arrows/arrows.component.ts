220dbcc60960a7786599e832e80329b0b86e693d
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-04 17:30:15 +0200

    200svg demo, driven by form grouo, but no form display yet

--- demo/src/app/content/arrow/arrows/arrows.component.ts
+++ demo/src/app/content/arrow/arrows/arrows.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import {SimplePlaygroundComponent} from "../../../layout/simple-playground/simple-playground.component";
 import {ArrowBetweenPointsComponent} from "ngx-emfular-diagram";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'demo-arrows',
   imports: [
     SimplePlaygroundComponent,
-    ArrowBetweenPointsComponent
+    ArrowBetweenPointsComponent,
   ],
   templateUrl: './arrows.component.html',
   styleUrl: './arrows.component.css'
 })
 export class ArrowsComponent {
+
+  form0 = new FormGroup({
+    startX: new FormControl(10,
+        {
+          nonNullable: true,
+          validators: [
+            Validators.min(0),
+            Validators.max(200)
+          ]
+        }),
+    startY: new FormControl(100, { nonNullable: true }),
+    endX: new FormControl(80, { nonNullable: true }),
+    endY: new FormControl(180, { nonNullable: true })
+  });
 
 }
