70560f50d3f6990d53ecfcfbc7e6c8c4e3e59070
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-04 15:03:11 +0200

    Introdcution to arrows plus simple playground: I need to provide the component code and a rendered form, also the form definition and  the initial form values and the string representation of the code for display.

---
+++ demo/src/app/layout/simple-playground/simple-playground.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input} from '@angular/core';
+import {AbstractControl, FormGroup} from "@angular/forms";
+
+type FormValue<TControls extends Record<string, AbstractControl<any, any>>> = {
+  [K in keyof TControls]:
+  TControls[K] extends AbstractControl<infer TValue, any>
+      ? TValue
+      : never;
+};
+
+@Component({
+  selector: 'demo-simple-playground',
+  imports: [],
+  templateUrl: './simple-playground.component.html',
+  styleUrl: './simple-playground.component.scss'
+})
+export class SimplePlaygroundComponent<TInputs extends{ [K in keyof TInputs]: AbstractControl<any, any>}> {
+  @Input() form!: FormGroup<TInputs>;
+  @Input() initialValue!: FormValue<TInputs>;
+  @Input() codeTemplate!: string;
+
+  reset(): void {
+    this.form.reset(this.initialValue);
+  }
+
+  copyCode(): void {
+    navigator.clipboard.writeText(this.codeTemplate);
+  }
+
+}
