581dd6877b0b2939e4605ff3120e53e9fbd99859
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-06 10:46:49 +0200

    Add code for arrow between boxes

--- demo/src/app/content/arrow/arrows/arrows.component.code.ts
+++ demo/src/app/content/arrow/arrows/arrows.component.code.ts
@@ -9,4 +9,37 @@ export const arrowBetweenPointsCode = `
 </svg:g>
 `;
 
-export const arrowBetweenBoxesCode = ``
+export const arrowBetweenBoxesCode = `<svg:g>
+        <g rectangleG
+           [position]="{
+                x: form1.value.box1X,
+                y: form1.value.box1Y,
+                w: form1.value.box1W,
+                h: form1.value.box1H
+            }"
+           color="red">
+        </g>
+        <g arrow-between-boxes
+           [start]="{
+                x: form1.value.box1X,
+                y: form1.value.box1Y,
+                w: form1.value.box1W,
+                h: form1.value.box1H
+            }"
+           [end]="{
+                x: form1.value.box2X,
+                y: form1.value.box2Y,
+                w: form1.value.box2W,
+                h: form1.value.box2H
+           }">
+        </g>
+        <g rectangleG
+           [position]="{
+                x: form1.value.box2X,
+                y: form1.value.box2Y,
+                w: form1.value.box2W,
+                h: form1.value.box2H
+            }"
+           color="blue">
+        </g>
+    </svg:g>`
