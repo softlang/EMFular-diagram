d1ff98b3e59ea09917d375b63d9511ead921b7f2
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-11 13:45:38 +0200

    Vitest setup

--- demo/src/app/layout/header/header.component.ts
+++ demo/src/app/layout/header/header.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'demo-header',
   imports: [
     RouterLink,
     RouterLinkActive,
-    NgOptimizedImage
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
---
+++ demo/vitest.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
+import angular from "@analogjs/vite-plugin-angular";
+
+//we use only browser config since the lib is mostly for graphical components and hence heavily depends on the DOM
+
+export default defineConfig(({mode})=>({
+    plugins: [
+        angular()
+    ],
+    resolve: mode === 'development'
+        ? {
+            alias: {
+                'ngx-emfular-diagram': '../src/public-api.ts'
+            }
+        }
+        : {},
+    optimizeDeps: {
+        include: [
+            '@angular/compiler',
+            '@angular/core',
+            '@angular/common',
+            '@angular/forms',
+            '@angular/core/testing',
+            '@angular/platform-browser-dynamic/testing',
+            'zone.js',
+            'zone.js/testing'
+        ]
+    },
+
+    test: {
+        include: ['src/**/*.spec.ts'],
+        exclude: [
+            'node_modules/**',
+            'dist/**',
+            'demo/**'
+        ],
+        globals: true,
+        setupFiles: ['./vitest.setup.ts'],
+        browser: {
+            enabled: true,
+            provider: playwright(),
+            // https://vitest.dev/config/browser/playwright
+            instances: [
+                {browser: 'chromium'}
+            ],
+        },
+    },
+}))
