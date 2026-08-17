7205bf9380c084ed409597cabf8f733f7d9ace08
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-17 15:48:14 +0200

    Describing dragging mechanism, even showing component code meant for extension

--- demo/src/app/content/drag-drop/drag-drop.component.ts
+++ demo/src/app/content/drag-drop/drag-drop.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {HighlightedCodeComponent} from "../../layout/highlighted-code/highlighted-code.component";
+import {dragDropComponent} from "./drag-drop.component.code";
+import {SimplePlaygroundComponent} from "../../layout/simple-playground/simple-playground.component";
 
 @Component({
   selector: 'demo-drag-drop',
-  imports: [],
+  imports: [
+    HighlightedCodeComponent,
+    SimplePlaygroundComponent
+  ],
   templateUrl: './drag-drop.component.html',
   styleUrl: './drag-drop.component.css'
 })
 export class DragDropComponent {
 
+  protected readonly dragDropComponent = dragDropComponent;
 }
