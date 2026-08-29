22e4ff828823843a2ca1ec45ad4a238d0b4163e1
Susanne Göbel <goebel@uni-koblenz.de>
2026-08-29 12:34:22 +0200

    Explain canvas

--- demo/src/app/content/home/home.component.ts
+++ demo/src/app/content/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {SvgReactivePlaygroundComponent} from "../../layout/svg-reactive-playground/svg-reactive-playground.component";
 
 @Component({
   selector: 'demo-home',
-  imports: [
-      RouterLink,
-      RouterLinkActive
-  ],
+    imports: [
+        RouterLink,
+        RouterLinkActive,
+        SvgReactivePlaygroundComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
