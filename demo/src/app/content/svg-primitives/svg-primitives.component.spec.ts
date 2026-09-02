337680a5f57de18dc5c4b1eb120e090a76467af2
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-02 10:13:47 +0200

    Fix demo test

--- demo/src/app/content/svg-primitives/svg-primitives.component.spec.ts
+++ demo/src/app/content/svg-primitives/svg-primitives.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SvgPrimitivesComponent } from './svg-primitives.component';
+import {ActivatedRoute} from "@angular/router";
 
 describe('SvgPrimitivesComponent', () => {
   let component: SvgPrimitivesComponent;
@@ -8,7 +9,10 @@ describe('SvgPrimitivesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SvgPrimitivesComponent]
+      imports: [SvgPrimitivesComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: {} }
+      ]
     })
     .compileComponents();
 
